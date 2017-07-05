ee7507c9d5856aa06a9959d26cef756e9a6c3935
Reshma MN <reshmanandagopal@gmail.com>
2017-07-05 18:39:20 +0530

    Update interlaceOneToN.c

--- interlaceOneToN.c
+++ interlaceOneToN.c
@@ -1,1 +1,15 @@
+/*1 to N forward and reverse interlaced*/
 
+#include<stdio.h>
+
+int main()
+{
+    int i=1,n;
+    scanf("%d",&n);
+    while(n != 0)
+    {
+        printf("%d %d ",i++,n--);
+    }
+    return 0;
+}
+    
